25e4fdbe3bdf55d730ee23af378ec05e6c1cd5ee
Khushi Nanwani <khushi.nanwani18@gmail.com>
2022-12-02 14:33:37 +0530

    added quiz app

---
+++ QuizYourself/app/src/main/java/com/example/quizyourself/QuestionAnswer.java
@@ -0,0 +1,44 @@
+package com.example.quizyourself;
+
+public class QuestionAnswer {
+
+    public static String question[]={
+            "What is the capital of India?",
+            "Who is the current Minister of Defense",
+            "The Highest peak in South India is",
+            "Red Cross was founded by",
+            "The gas used to extinguish fire is",
+            "The largest part of Human brain is",
+            "Which of the following is the lightest gas?",
+            "The largest planet of the solar system is ",
+            "The first synthetic fibre made by man is",
+            "The orbit along which the Earth is approximately?"
+    };
+
+    public static String choices[][]={
+            {"Mumbai","Kolkata","Delhi","Pune"},
+            {"Rajnath Singh","Nirmala Sitharaman","Shri Dharmendra Pradhan","Shri Nitin Jairam Gadkari"},
+            {"Everest","Doddabetta","Nilgiri hills","Anaimudi"},
+            {"A.Cursetji","Badel Powell","J.H.Durant","Trygve Lie"},
+            {"Carbon Monoxide","Neon","Nitrogen","Carbon Dioxide"},
+            {"Cerebellum","Midbrain","Cerebrum","Medulla Oblongata"},
+            {"Ammonia","Carbon Dioxide","Nitrogen","Hydrogen"},
+            {"Mars","Earth","Jupiter","Venus"},
+            {"Polyster","Rayon","Terrycloth","Nylon"},
+            {"Irregular","Elliptical","Circular","Rectangular"}
+    };
+
+    public static String correctAnswers[]={
+            "Delhi",
+            "Rajnath Singh",
+            "Anaimudi",
+            "J.H.Durant",
+            "Carbon Dioxide",
+            "Cerebrum",
+            "Hydrogen",
+            "Jupiter",
+            "Nylon",
+            "Elliptical"
+    };
+
+}
